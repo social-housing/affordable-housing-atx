9cd369a8bc9c8a88d6fe2946409ed6889540eada
sdcaulley <stephanie.caulley@gmail.com>
2019-09-21 18:28:34 -0500

    data displayed on dom

--- scripts/api.js
+++ scripts/api.js
@@ -1,32 +1,26 @@
 let button = document.querySelector('#submit');
 
-function callApi(request) {
-  const url = makeUrl(request);
-  console.log('url: ', url);
-  $.ajax({
-    url: url,
-    type: 'GET',
-    data: {
-      '$limit' : 5000,
-      '$$app_token' : 'R95CM3pwSQ746G08aNK2PQPO1'
-    }
-  })
-    .done(function(data) {
-      console.log('data: ', data);
-      return data;
-    });
-}
-
 function makeUrl(request) {
   const zip_code = request.zip_code;
   const url = `https://data.austintexas.gov/resource/ngxp-99y3.json?zip_code=${zip_code}`;
   return url;
 }
 
-// function resultDisplay(data) {
-//   const resultDiv = document.querySelector('#result-display');
-//   const resultUl = <ul></ul>
-// }
+
+
+function locationDisplay(items) {
+  const results = items.map(item => {
+    return `<li>
+      ${item.project_name}<br> ${item.address} ${item.zip_code}
+    </li>`
+  }).join('\n');
+  return `<ul>${results}</ul>`;
+}
+
+function resultDisplay(data) {
+  const resultDiv = document.querySelector('#result-display');
+  resultDiv.innerHTML = locationDisplay(data);
+}
 
 function householdValue() {
   let ele = document.getElementsByName('household-number');
@@ -39,7 +33,23 @@ function householdValue() {
   return result;
 }
 
-button.addEventListener('click', (event) => {
+function callApi(request) {
+  const url = makeUrl(request);
+  $.ajax({
+    url: url,
+    type: 'GET',
+    data: {
+      '$limit' : 5000,
+      '$$app_token' : 'R95CM3pwSQ746G08aNK2PQPO1'
+    }
+  })
+    .done(function(data) {
+      console.log('data: ', data);
+      resultDisplay(data);
+    });
+}
+
+button.addEventListener('click', async (event) => {
   event.preventDefault();
 
   const zip = parseInt(document.querySelector('#zip-code').value);
@@ -50,6 +60,5 @@ button.addEventListener('click', (event) => {
     household: household,
     income: income
   };
-  const result = callApi(request);
-  console.log('result: ', result);
+  callApi(request);
 });
